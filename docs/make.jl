ccbd697ab9fe0e71fee3516b7927eef876ff996c
Charles Kawczynski <kawczynski.charles@gmail.com>
2021-03-18 17:57:52 -0700

    Add infra

---
+++ docs/make.jl
@@ -0,0 +1,27 @@
+using Documenter, NoLimitHoldem
+
+format = Documenter.HTML(
+    prettyurls = !isempty(get(ENV, "CI", "")),
+    collapselevel = 1,
+)
+
+makedocs(
+    sitename = "NoLimitHoldem.jl",
+    strict = true,
+    format = format,
+    checkdocs = :exports,
+    clean = true,
+    doctest = true,
+    modules = [NoLimitHoldem],
+    pages = Any[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(
+    repo = "github.com/charleskawczynski/NoLimitHoldem.jl.git",
+    target = "build",
+    push_preview = true,
+    devbranch = "main",
+    forcepush = true,
+)
