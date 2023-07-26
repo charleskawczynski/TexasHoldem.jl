3a18bb28e2c52ed6e90c8dc63fcde32ec98ba844
Charles Kawczynski <kawczynski.charles@gmail.com>
2023-07-26 13:43:14 -0700

    Fix ci for older julia versions

--- test/reproducibility.jl
+++ test/reproducibility.jl
@@ -4,7 +4,7 @@ import Random
 
 function seeded_game(;fun,n_players, n_games, bank_roll=200)
     Random.seed!(1234)
-    players = ntuple(i->Player(Bot5050(), i; bank_roll), n_players)
+    players = ntuple(i->Player(Bot5050(), i; bank_roll=bank_roll), n_players)
     games = map(x->Game(deepcopy(players);logger=TexasHoldem.ByPassLogger()), 1:n_games)
     for n in 1:length(games)
         game = games[n]
@@ -16,8 +16,8 @@ end
 @testset "Reproducibility for play!" begin
     n_games = 1000
     n_players = 10
-    games1 = seeded_game(;fun=play!,n_players, n_games, bank_roll=200)
-    games2 = seeded_game(;fun=play!,n_players, n_games, bank_roll=200)
+    games1 = seeded_game(;fun=play!,n_players=n_players, n_games=n_games, bank_roll=200)
+    games2 = seeded_game(;fun=play!,n_players=n_players, n_games=n_games, bank_roll=200)
     for (g1, g2) in zip(games1, games2)
         for (p1, p2) in zip(g1.table.players, g2.table.players)
             @test bank_roll(p1) == bank_roll(p2)
@@ -28,8 +28,8 @@ end
 @testset "Reproducibility for tournament!" begin
     n_games = 10000
     n_players = 3
-    games1 = seeded_game(;fun=tournament!,n_players, n_games, bank_roll=200)
-    games2 = seeded_game(;fun=tournament!,n_players, n_games, bank_roll=200)
+    games1 = seeded_game(;fun=tournament!,n_players=n_players, n_games=n_games, bank_roll=200)
+    games2 = seeded_game(;fun=tournament!,n_players=n_players, n_games=n_games, bank_roll=200)
     for (g1, g2) in zip(games1, games2)
         for (p1, p2) in zip(g1.table.players, g2.table.players)
             @test bank_roll(p1) == bank_roll(p2)
@@ -38,8 +38,8 @@ end
 
     n_games = 1000
     n_players = 10
-    games1 = seeded_game(;fun=tournament!,n_players, n_games, bank_roll=200)
-    games2 = seeded_game(;fun=tournament!,n_players, n_games, bank_roll=200)
+    games1 = seeded_game(;fun=tournament!,n_players=n_players, n_games=n_games, bank_roll=200)
+    games2 = seeded_game(;fun=tournament!,n_players=n_players, n_games=n_games, bank_roll=200)
     for (g1, g2) in zip(games1, games2)
         for (p1, p2) in zip(g1.table.players, g2.table.players)
             @test bank_roll(p1) == bank_roll(p2)
